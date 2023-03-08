bd222b4b9fb7dea4620e9922e4fd41d90510293d
Jack Nam <81426591+jacknam05@users.noreply.github.com>
2023-03-08 16:23:14 +0700

    Update heart.js

--- js/heart.js
+++ js/heart.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana";
-            ctx.fillText("Bấm vào hình trái tym đi", 23, 10);
+            ctx.fillText("Bấm vào trái tym đi", 23, 10);
             ctx.restore();
         },
         clear: function() {
